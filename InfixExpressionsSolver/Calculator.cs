4469741adb1a4b286755c48f157813edc0d4ad06
Camila Kutscher <cakutscher@uc.cl>
2018-09-08 19:06:36 -0300

    implement solution

---
+++ InfixExpressionsSolver/Calculator.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace InfixExpressionsSolver
+{
+    static class Calculator
+    {
+        /*
+         * Algorithm adapted from a solution proposed in stackoverflow https://stackoverflow.com/a/44268877.
+         * For better performance, convert infix expression into postfix before solving.
+         */
+        public static int Evaluate(Array infix)
+        {
+            var operatorstack = new Stack<string>();
+            var operandstack = new Stack<int>();
+            var precedence = new Dictionary<string, int> { { "(", 0 }, { "*", 1 }, { "/", 1 }, { "-", 2 }, { "+", 3 }, { ")", 4 } };
+
+            foreach (string ch in infix)
+            {
+                switch (ch)
+                {
+                    case var digit when int.TryParse(ch, out int n):
+                        operandstack.Push(Convert.ToInt32(digit.ToString()));
+                        break;
+                    case var op when precedence.ContainsKey(op):
+                        var keepLooping = true;
+                        while (keepLooping && operatorstack.Count > 0 && precedence[ch] > precedence[operatorstack.Peek()])
+                        {
+                            switch (operatorstack.Peek())
+                            {
+                                case "+":
+                                    operandstack.Push(operandstack.Pop() + operandstack.Pop());
+                                    break;
+                                case "-":
+                                    operandstack.Push(-operandstack.Pop() + operandstack.Pop());
+                                    break;
+                                case "*":
+                                    operandstack.Push(operandstack.Pop() * operandstack.Pop());
+                                    break;
+                                case "/":
+                                    var divisor = operandstack.Pop();
+                                    operandstack.Push(operandstack.Pop() / divisor);
+                                    break;
+                                case "(":
+                                    keepLooping = false;
+                                    break;
+                            }
+                            if (keepLooping)
+                            {
+                                operatorstack.Pop();
+                            }
+
+
+                        }
+                        if (ch == ")")
+                        {
+                            operatorstack.Pop();
+                        }
+                        else
+                        {
+                            operatorstack.Push(ch);
+                        }
+
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+
+            }
+
+            if (operatorstack.Count > 0 || operandstack.Count > 1)
+                throw new ArgumentException();
+
+            return operandstack.Pop();
+
+        }
+
+        public static void CheckBalance(String secuence)
+        {
+            Stack<char> stack = new Stack<char>();
+
+
+            foreach (char c in secuence)
+            {
+
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                    continue;
+                }
+                switch (c)
+                {
+
+                    case ')':
+                        if (stack.Count == 0 || stack.Pop() != '(')
+                        {
+                            throw new ArgumentException();
+                        }
+
+                        break;
+                    case '}':
+                        if (stack.Count == 0 || stack.Pop() != '{')
+                        {
+                            throw new ArgumentException();
+                        }
+
+                        break;
+                    case ']':
+                        if (stack.Count == 0 || stack.Pop() != '[')
+                        {
+                            throw new ArgumentException();
+                        }
+
+                        break;
+                }
+            }
+            if (stack.Count != 0)
+                throw new ArgumentException();
+        }
+
+    }
+}
---
+++ InfixExpressionsSolver/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace InfixExpressionsSolver
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" ** Please enter the path to your file **");
+            Console.Write("Path: ");
+            string pth = Console.ReadLine();
+            string txt = System.IO.File.ReadAllLines(pth)[0];
+
+            String secuence = txt.Replace('{', '(').Replace('[', '(').Replace('}', ')').Replace(']', ')');
+            secuence = "(" + secuence + ")";
+            string[] infix = Regex.Split(secuence, @"(?<=[{}()+*/-])|(?=[{}()+*/-])").Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            try
+            {
+                Calculator.CheckBalance(txt);
+                int result = Calculator.Evaluate(infix);
+                Console.WriteLine("Output: " + result);
+
+            }
+            catch
+            {
+                Console.WriteLine("Syntax Error");
+            }
+        }
+    }
+}
